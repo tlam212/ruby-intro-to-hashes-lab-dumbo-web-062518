649e9614ee2cf0defaf9d9a84ea15e015539062d
Toni Lamont <tlam212@gmail.com>
2018-06-19 21:00:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def monopoly
 	monopoly = {
 	  :railroads => {pieces: "4", rent_in_dollars:{
-	    one_piece_ownded: "25", two_pieces_owned: "50", three_pieces_owned: "100"
+	    one_piece_ownded: "25", two_pieces_owned: "50", three_pieces_owned: "100", four_pieces_owned: "200"
 	  }},
 	  :names => {}
 	}
