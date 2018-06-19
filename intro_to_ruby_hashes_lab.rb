323b2f21c95dbe55df45c0fae05fbf030f43f138
Toni Lamont <tlam212@gmail.com>
2018-06-19 21:19:39 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,11 +18,10 @@ def monopoly
 	    :three_pieces_owned => 100, :four_pieces_owned => 200},
 
 	  :names => {
-	    :reading_railroad => {
-	      "mortgage_value": "$100"}, 
-	      :pennsylvania_railroad => {"mortgag_value": "$200"},
-	      :b_and_o_railroad => {"mortgage_value": "$400"}, 
-	      :shortline => {"mortgage_value": "$800"}
+	    :reading_railroad => {"mortgage_value": "$100"}, 
+	    :pennsylvania_railroad => {"mortgag_value": "$200"},
+	    :b_and_o_railroad => {"mortgage_value": "$400"}, 
+	    :shortline => {"mortgage_value": "$800"}
 	    
 	  }
 	}
