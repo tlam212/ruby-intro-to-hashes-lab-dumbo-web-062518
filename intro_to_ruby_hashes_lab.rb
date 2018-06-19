c14da797198006405ef1cb534bf91fc4b67bee81
Toni Lamont <tlam212@gmail.com>
2018-06-19 21:30:56 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_second_tier
 	  :railroads => {pieces: 4,
 	    rent_in_dollars:{}
 	  },
-	  :names => {}
+	  :pieces => {}
 } 
 	
 end
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 	  :railroads => {pieces: 4,
 	    rent_in_dollars:{}
 	  },
-	  :names => {}
+	  :na => {}
 } 
 end
 
