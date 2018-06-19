9efc7aac39a858d5e08112ebe54bfb0f06e5e74e
Toni Lamont <tlam212@gmail.com>
2018-06-19 21:49:48 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ def monopoly_with_fourth_tier
 		monopoly = {
 	  :railroads => {pieces: 4, 
 	  names:{
-	    reading_railroad:{
+	    :reading_railroad =>{
 	      "mortgage_value": "$100"
 	    }, 
 	    pennsylvania_railroad:{
