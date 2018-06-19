b78967d55d84a05d7dccdf09e31e4aa545a7094b
Toni Lamont <tlam212@gmail.com>
2018-06-19 21:33:20 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,12 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-		
+			monopoly = {
+	  :railroads => {pieces: 4, names:{}, rent_in_dollars:{}
+	   
+	  },
+	  :pieces => {}
+} 
 end
 
 def monopoly_with_fourth_tier
